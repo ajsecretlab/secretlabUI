bad812170f689484354594bbae2f053aaff58b4f
ajsecretlab <104127893+ajsecretlab@users.noreply.github.com>
2023-01-27 15:08:09 +0800

    Add vite and storybook

---
+++ src/entry.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>To run storybook: npm run storybook</div>;
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Entry from "./entry";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <Entry />
+  </React.StrictMode>
+);
