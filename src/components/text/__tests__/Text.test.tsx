ec43d3cc68970075bba0b323a99c19c6522c3c10
ajsecretlab <104127893+ajsecretlab@users.noreply.github.com>
2023-01-30 11:57:17 +0800

    Add vitest for testing component with jestdom for react DOM testing

---
+++ src/components/text/__tests__/Text.test.tsx
@@ -0,0 +1,20 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { Text } from "../index";
+
+describe("App", () => {
+  it("should work as expected", () => {
+    render(<Text name="test" />);
+    expect(1 + 1).toBe(2);
+  });
+});
+
+describe("something truthy and falsy", () => {
+  it("true to be true", () => {
+    expect(true).toBe(true);
+  });
+
+  it("false to be false", () => {
+    expect(false).toBe(false);
+  });
+});
