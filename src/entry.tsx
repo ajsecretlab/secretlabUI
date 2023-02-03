c4c66c379298691540bbbd4f5f7dfa43f9bf8fa6
ajsecretlab <104127893+ajsecretlab@users.noreply.github.com>
2023-02-03 17:24:30 +0800

    Remove text component

--- src/entry.tsx
+++ src/entry.tsx
@@ -1,5 +1,3 @@
-import { Text } from "./components/text";
-
 function Entry() {
   return (
     <div className="mt-20 m-auto w-[40%] text-center border border-gray-500 p-5 rounded shadow">
@@ -7,7 +5,6 @@ function Entry() {
         You can view components using storybook
       </h1>
       <p>To run storybook: npm run storybook</p>
-      <Text name="test" />
     </div>
   );
 }
